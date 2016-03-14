02568ce2668ba312efb6689f1b71f6b62636e377
Max <maxruizee@gmail.com>
2016-03-14 10:54:22 -0700

    Update point.py

--- point.py
+++ point.py
@@ -3,40 +3,27 @@ Created on Mar 6, 2016
 
 @author: Max Ruiz
 '''
-import unittest
-from point import Point
-
-class TestPointClass(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def test_set_coords(self):
-        _x = 5
-        _y = 120
-        point0 = Point(_x, _y)
-        self.assertEqual(_x, point0.getx())
-        self.assertEqual(_y, point0.gety())
-
-    def test_check_coincident(self):
-        _x = 5
-        _y = 120
-        point0 = Point(_x, _y)
-        point1 = Point(5, 120)
-        point2 = Point(_x+3, _y)
-        point3 = Point(_x, _y+22)
-        point4 = Point(_x+3,_y+22)
-
-        self.assertTrue(point0.check_coincident(point1.getPoint()))
-        self.assertFalse(point0.check_coincident(point2.getPoint()))
-        self.assertFalse(point0.check_coincident(point3.getPoint()))
-        self.assertFalse(point0.check_coincident(point4.getPoint()))
-
-    def test_shift(self):
-        _x = 5
-        _y = 120
-        point0 = Point(_x, _y)
-        point0.shift_point(816, 80085)
-        self.assertEqual((_x + 816, _y + 80085), point0.getPoint())
 
 
+class Point(object):
 
+    def __init__(self, x, y, **mark):
+        self.x = x
+        self.y = y
+        self.mark = mark
+
+    def check_coincident(self, point):
+        return (self.x == point[0] and self.y == point[1])
+
+    def shift_point(self, x_shift, y_shift):
+        self.x += x_shift
+        self.y += y_shift
+
+    def getx(self):
+        return self.x
+
+    def gety(self):
+        return self.y
+
+    def getPoint(self):
+        return (self.x, self.y)
