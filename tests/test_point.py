9251d6441b85977efe48f04e368c737b44b485ef
Max <maxruizee@gmail.com>
2016-03-14 20:53:33 -0700

    Update test_point.py

--- tests/test_point.py
+++ tests/test_point.py
@@ -5,7 +5,8 @@ Created on Mar 6, 2016
 '''
 import unittest
 import random
-from point import Point
+#from point import Point
+from .. import point
 
 class TestPointClass(unittest.TestCase):
     def setUp(self):
@@ -14,18 +15,18 @@ class TestPointClass(unittest.TestCase):
     def test_set_coords(self):
         _x = 5
         _y = 120
-        point0 = Point(_x, _y)
+        point0 = point.Point(_x, _y)
         self.assertEqual(_x, point0.getx())
         self.assertEqual(_y, point0.gety())
 
     def test_check_coincident(self):
         _x = 5
         _y = 120
-        point0 = Point(_x, _y)
-        point1 = Point(5, 120)
-        point2 = Point(_x+3, _y)
-        point3 = Point(_x, _y+22)
-        point4 = Point(_x+3,_y+22)
+        point0 = point.Point(_x, _y)
+        point1 = point.Point(5, 120)
+        point2 = point.Point(_x+3, _y)
+        point3 = point.Point(_x, _y+22)
+        point4 = point.Point(_x+3,_y+22)
 
         self.assertTrue(point0.check_coincident(point1.getPoint()))
         self.assertFalse(point0.check_coincident(point2.getPoint()))
@@ -35,7 +36,7 @@ class TestPointClass(unittest.TestCase):
     def test_shift(self):
         _x = 5
         _y = 120
-        point0 = Point(_x, _y)
+        point0 = point.Point(_x, _y)
         point0.shift_point(816, 80085)
         self.assertEqual((_x + 816, _y + 80085), point0.getPoint())
 
